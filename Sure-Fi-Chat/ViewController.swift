2b4dd445fac2be4a459c0006ebbd4370e0d8507d
Sure-Fi Inc <sure-fi@Sure-Fis-iMac.local>
2017-11-16 14:03:11 -0700

    Initial Commit

---
+++ Sure-Fi-Chat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sure-Fi-Chat
+//
+//  Created by Sure-Fi Inc. on 11/16/17.
+//  Copyright © 2017 Sure-Fi Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
